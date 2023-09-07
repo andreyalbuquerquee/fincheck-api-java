d4653868af5a6cd655e12982bff226aeb0b20931
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-07 09:01:51 -0300

    setting email column from user as unique

--- src/main/java/com/fincheck/fincheckapijava/model/User.java
+++ src/main/java/com/fincheck/fincheckapijava/model/User.java
@@ -20,7 +20,7 @@ public class User {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     private String email;
     
     @Column(name = "password")
