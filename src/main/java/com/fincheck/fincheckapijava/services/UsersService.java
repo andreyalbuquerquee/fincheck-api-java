4d0b26f30ba6efe84ebf5fe3f874835a90c7f2b7
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-08 08:36:48 -0300

    users service login

--- src/main/java/com/fincheck/fincheckapijava/services/UsersService.java
+++ src/main/java/com/fincheck/fincheckapijava/services/UsersService.java
@@ -1,16 +1,23 @@
 package com.fincheck.fincheckapijava.services;
 
+import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.fincheck.fincheckapijava.model.User;
 import com.fincheck.fincheckapijava.repository.UsersRepository;
+import com.fincheck.fincheckapijava.security.JWTService;
+import com.fincheck.fincheckapijava.view.model.user.LoginResponse;
 
 @Service
 public class UsersService {
@@ -19,6 +26,12 @@ public class UsersService {
 
     @Autowired 
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JWTService jwtService;
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
     
     public User create(User user) {
         user.setId(null);
@@ -43,5 +56,20 @@ public class UsersService {
     public Optional<User> getByEmail(String email) {
         return usersRepo.findByEmail(email);
     }
+
+    public LoginResponse login(String email, String password) {
+        Authentication auth = authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(email, password, Collections.emptyList()));
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String token = headerPrefix + jwtService.generateToken(auth).substring(7);
+
+        User user = usersRepo.findByEmail(email).get();
+
+        return new LoginResponse(token, user);
+    }
+
+    private static final String headerPrefix = "Bearer";
     
 }
