e04abcb03e1242a8aa195fa577dc553c3970d082
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-08 10:44:42 -0300

    auth controller

---
+++ src/main/java/com/fincheck/fincheckapijava/view/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.fincheck.fincheckapijava.view.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fincheck.fincheckapijava.model.User;
+import com.fincheck.fincheckapijava.services.UsersService;
+import com.fincheck.fincheckapijava.view.model.user.LoginRequest;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    
+    @Autowired
+    private UsersService service;
+    
+    @PostMapping("/signup")
+    public ResponseEntity<Object> signup(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(user));
+    }
+    
+    @PostMapping("/signin")
+    public ResponseEntity<Object> signin(@RequestBody LoginRequest login) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.login(login.getEmail(), login.getPassword()));
+    }
+}
