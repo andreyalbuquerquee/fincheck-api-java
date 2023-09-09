8ed9ce98b616365e5749f5d890630dbe38d8bc2d
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-09 20:28:17 -0300

    test route

--- src/main/java/com/fincheck/fincheckapijava/controller/AuthController.java
+++ src/main/java/com/fincheck/fincheckapijava/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.fincheck.fincheckapijava.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +29,10 @@ public class AuthController {
     @PostMapping("/signin")
     public ResponseEntity<Object> signin(@RequestBody @Valid SigninDto signinDto) {
         return ResponseEntity.status(HttpStatus.OK).body(service.signin(signinDto));
+    }
+
+    @GetMapping("/teste")
+    public ResponseEntity<Object> teste() {
+        return ResponseEntity.status(HttpStatus.OK).body("Funcionou");
     }
 }
