483756166fdaac4f8ba8ecc67eb077aad8612b8f
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-11 05:46:34 -0300

    Returning 401 unauthorized when invalid bearer token

--- src/main/java/com/fincheck/fincheckapijava/security/WebSecurityConfig.java
+++ src/main/java/com/fincheck/fincheckapijava/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -35,6 +37,7 @@ public class WebSecurityConfig {
         .requestMatchers(HttpMethod.POST, "/auth/signup").permitAll()
         .requestMatchers(HttpMethod.POST, "/auth/signin").permitAll()
         .anyRequest().authenticated())
+        .exceptionHandling(t -> t.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
         // Before every httpRequest, we shoul use our own filters
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
