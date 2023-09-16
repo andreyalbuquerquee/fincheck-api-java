574bf1836f1878a71eb118d3d9f79652d5a482d5
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-16 02:47:30 -0300

    notFoundException extends RuntimeException

--- src/main/java/com/fincheck/fincheckapijava/exceptions/NotFoundException.java
+++ src/main/java/com/fincheck/fincheckapijava/exceptions/NotFoundException.java
@@ -1,21 +1,10 @@
 package com.fincheck.fincheckapijava.exceptions;
 
-import org.springframework.http.HttpStatus;
 
-public class NotFoundException {
-    private final String message;
-    private final HttpStatus httpStatus;
-    
-    
-    public NotFoundException(String message, HttpStatus httpStatus) {
-        this.message = message;
-        this.httpStatus = httpStatus;
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
     }
-    
-    public String getMessage() {
-        return message;
-    }
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
+   
 }
