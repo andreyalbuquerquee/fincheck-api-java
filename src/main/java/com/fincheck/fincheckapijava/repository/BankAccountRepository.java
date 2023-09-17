bc08c27fceb74b027db304bd0b0d6279b5daf44b
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-17 07:12:36 -0300

    bankAccountRepository - find by id and user method

--- src/main/java/com/fincheck/fincheckapijava/repository/BankAccountRepository.java
+++ src/main/java/com/fincheck/fincheckapijava/repository/BankAccountRepository.java
@@ -5,7 +5,11 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fincheck.fincheckapijava.model.BankAccount;
+import com.fincheck.fincheckapijava.model.User;
+
+import java.util.Optional;
+
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
-    
+    Optional<BankAccount> findByIdAndUser(UUID id, User user);
 }
