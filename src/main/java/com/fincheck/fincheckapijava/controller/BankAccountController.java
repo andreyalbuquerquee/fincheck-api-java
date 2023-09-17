3ef35efe68398bdac700b266570a5e5f4a21a3a2
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-17 07:11:46 -0300

    update endpoint

--- src/main/java/com/fincheck/fincheckapijava/controller/BankAccountController.java
+++ src/main/java/com/fincheck/fincheckapijava/controller/BankAccountController.java
@@ -1,9 +1,13 @@
 package com.fincheck.fincheckapijava.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +27,12 @@ public class BankAccountController {
     
     @PostMapping()
     public ResponseEntity<Object> create(@RequestHeader(value = "Authorization") String accessToken, @RequestBody @Valid BankAccountDto createBankAccountDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(bankAccountService.create(accessToken, createBankAccountDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(bankAccountService.create(accessToken, createBankAccountDto));
+    }
+
+    @PutMapping("/{bankAccountId}")
+    public ResponseEntity<Object> update(@RequestHeader(value = "Authorization") String accessToken, @PathVariable UUID bankAccountId, @RequestBody @Valid BankAccountDto updateBankAccountDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(bankAccountService.update(accessToken, bankAccountId, updateBankAccountDto));
     }
     
 }
