96eecf772e197046887931f824f1efabd87e27c5
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-19 20:40:11 -0300

    CategoriesRepository

--- src/main/java/com/fincheck/fincheckapijava/repository/CategoriesRepository.java
+++ src/main/java/com/fincheck/fincheckapijava/repository/CategoriesRepository.java
@@ -1,11 +1,13 @@
 package com.fincheck.fincheckapijava.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fincheck.fincheckapijava.model.Category;
+import com.fincheck.fincheckapijava.model.User;
 
 public interface CategoriesRepository extends JpaRepository<Category, UUID> {
-    
+    List<Category> findAllByUser(User user);
 }
