588483a13c32ec19bfa04c7b3ea553be90811f6c
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-20 19:25:23 -0300

    ValidateTransactionType annotation

---
+++ src/main/java/com/fincheck/fincheckapijava/validation/ValidateTransactionType.java
@@ -0,0 +1,29 @@
+package com.fincheck.fincheckapijava.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = TransactionTypeValidator.class)
+public @interface ValidateTransactionType {
+    String message();
+
+    Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default { };
+}
