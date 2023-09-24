8c436d1d8d099e2acab4b4a9afa4ad437d91dd15
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-23 22:53:49 -0300

    transactionController - findAll

--- src/main/java/com/fincheck/fincheckapijava/controller/TransactionController.java
+++ src/main/java/com/fincheck/fincheckapijava/controller/TransactionController.java
@@ -3,12 +3,15 @@ package com.fincheck.fincheckapijava.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fincheck.fincheckapijava.model.enums.TransactionType;
 import com.fincheck.fincheckapijava.services.TransactionService;
 import com.fincheck.fincheckapijava.shared.dtos.TransactionDto;
 
@@ -22,5 +25,16 @@ public class TransactionController {
     @PostMapping
     public ResponseEntity<Object> create(@RequestHeader(value = "Authorization") String accessToken, @RequestBody TransactionDto createTransactionDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.create(accessToken, createTransactionDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> findAll(
+        @RequestHeader(value = "Authorization") String accessToken, 
+        @RequestParam int month, 
+        @RequestParam int year,
+        @RequestParam(required = false) String bankAccountId,
+        @RequestParam(required = false) TransactionType type
+        ) {
+        return ResponseEntity.status(HttpStatus.OK).body(transactionService.findAllByUserId(accessToken, month, year, bankAccountId, type));
     }
 }
