800effff3d01a7dedf6f5e66d141211468f94f85
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-25 20:52:27 -0300

    findAll transactionController

--- src/main/java/com/fincheck/fincheckapijava/controller/TransactionController.java
+++ src/main/java/com/fincheck/fincheckapijava/controller/TransactionController.java
@@ -1,5 +1,7 @@
 package com.fincheck.fincheckapijava.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,23 +20,31 @@ import com.fincheck.fincheckapijava.shared.dtos.TransactionDto;
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
-    
+
     @Autowired
     TransactionService transactionService;
 
     @PostMapping
-    public ResponseEntity<Object> create(@RequestHeader(value = "Authorization") String accessToken, @RequestBody TransactionDto createTransactionDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.create(accessToken, createTransactionDto));
+    public ResponseEntity<Object> create(@RequestHeader(value = "Authorization") String accessToken,
+                                         @RequestBody TransactionDto createTransactionDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.create(
+                accessToken,
+                createTransactionDto));
     }
 
     @GetMapping
     public ResponseEntity<Object> findAll(
-        @RequestHeader(value = "Authorization") String accessToken, 
-        @RequestParam int month, 
-        @RequestParam int year,
-        @RequestParam(required = false) String bankAccountId,
-        @RequestParam(required = false) TransactionType type
-        ) {
-        return ResponseEntity.status(HttpStatus.OK).body(transactionService.findAllByUserId(accessToken, month, year, bankAccountId, type));
+            @RequestHeader(value = "Authorization") String accessToken,
+            @RequestParam int month,
+            @RequestParam int year,
+            @RequestParam(required = false) UUID bankAccountId,
+            @RequestParam(required = false) TransactionType type
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(transactionService.findAllByUserId(
+                accessToken,
+                month,
+                year,
+                bankAccountId,
+                type));
     }
 }
