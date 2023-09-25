905a0ef2a5aade168eaff544a3c259be2a3bac39
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-09-25 20:52:42 -0300

    findAllByUserId method transactionService

--- src/main/java/com/fincheck/fincheckapijava/services/TransactionService.java
+++ src/main/java/com/fincheck/fincheckapijava/services/TransactionService.java
@@ -1,6 +1,5 @@
 package com.fincheck.fincheckapijava.services;
 
-import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Optional;
 import java.util.TimeZone;
@@ -24,7 +23,7 @@ import com.fincheck.fincheckapijava.shared.dtos.TransactionDto;
 
 @Service
 public class TransactionService {
-    
+
     @Autowired
     UsersRepository usersRepo;
 
@@ -39,60 +38,76 @@ public class TransactionService {
 
     @Autowired
     JWTService jwtService;
-    
+
     public Transaction create(String accessToken, TransactionDto createTransactionDto) {
         Optional<UUID> currentUserId = jwtService.activeUserId(accessToken.substring(7));
         Optional<User> user = usersRepo.findById(currentUserId.get());
 
-        validateEntitiesOwnership(user.get(), createTransactionDto.bankAccountId(), createTransactionDto.categoryId());
+        validateEntitiesOwnership(user.get(), UUID.fromString(createTransactionDto.bankAccountId()),
+                createTransactionDto.categoryId());
 
-        Optional<BankAccount> bankAccount = bankAccountsRepo.findById(UUID.fromString(createTransactionDto.bankAccountId()));
+        Optional<BankAccount> bankAccount =
+                bankAccountsRepo.findById(UUID.fromString(createTransactionDto.bankAccountId()));
         Optional<Category> category = categoriesRepo.findById(UUID.fromString(createTransactionDto.categoryId()));
-        
+
         Transaction transaction = new Transaction(createTransactionDto, user.get(), bankAccount.get(), category.get());
-        
+
         return transactionsRepo.save(transaction);
     }
-    
+
     public Object findAllByUserId(
-        String accessToken, 
-        int month, 
-        int year, 
-        String bankAccountId, 
-        TransactionType type) {
-        
+            String accessToken,
+            int month,
+            int year,
+            UUID bankAccountId,
+            TransactionType type) {
+
         UUID currentUserId = jwtService.activeUserId(accessToken.substring(7)).get();
-        
+
         Calendar calendarDate = Calendar.getInstance();
         calendarDate.setTimeZone(TimeZone.getDefault());
-        
+
         calendarDate.set(Calendar.YEAR, year);
         calendarDate.set(Calendar.MONTH, month - 1);
         calendarDate.set(Calendar.DAY_OF_MONTH, 1);
-        
+
         calendarDate.set(Calendar.HOUR_OF_DAY, 0);
         calendarDate.set(Calendar.MINUTE, 0);
         calendarDate.set(Calendar.SECOND, 0);
         calendarDate.set(Calendar.MILLISECOND, 0);
 
-        Timestamp startDate = new Timestamp(calendarDate.getTimeInMillis());
+        Calendar startDate = Calendar.getInstance();
+        startDate.setTimeInMillis(calendarDate.getTimeInMillis());
         calendarDate.add(Calendar.MONTH, 1);
-        Timestamp endDate = new Timestamp(calendarDate.getTimeInMillis());
-        
+        Calendar endDate = Calendar.getInstance();
+        endDate.setTimeInMillis(calendarDate.getTimeInMillis());
+
+        if (bankAccountId != null && type != null) {
+            return transactionsRepo.findByUserAndDateAndBankAccountAndType(currentUserId, startDate, endDate,
+                    bankAccountId, type.name());
+        }
+
+        if (bankAccountId != null) {
+            return transactionsRepo.findByUserAndDateAndBankAccount(currentUserId, startDate, endDate, bankAccountId);
+        }
+
+        if (type != null) {
+            return transactionsRepo.findByUserAndDateAndType(currentUserId, startDate, endDate, type.name());
+        }
 
         return transactionsRepo.findByUserAndDate(currentUserId, startDate, endDate);
     }
-    
-    private void validateEntitiesOwnership(User user, String bankAccountId, String categoryId) {
+
+    private void validateEntitiesOwnership(User user, UUID bankAccountId, String categoryId) {
         Optional<Category> category = categoriesRepo.findByIdAndUser(UUID.fromString(categoryId), user);
 
-        if (!category.isPresent()) {
+        if (category.isEmpty()) {
             throw new NotFoundException("Category not found!");
         }
 
-        Optional<BankAccount> bankAccount = bankAccountsRepo.findByIdAndUser(UUID.fromString(bankAccountId), user);
+        Optional<BankAccount> bankAccount = bankAccountsRepo.findByIdAndUser(bankAccountId, user);
 
-        if (!bankAccount.isPresent()) {
+        if (bankAccount.isEmpty()) {
             throw new NotFoundException("Bank Account not found!");
         }
     }
