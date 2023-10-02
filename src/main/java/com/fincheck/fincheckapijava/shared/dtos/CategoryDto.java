aacf84a335e77ad19f6a7c50c3a9ad794648787f
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-10-02 17:28:41 -0300

    CategoryDto

---
+++ src/main/java/com/fincheck/fincheckapijava/shared/dtos/CategoryDto.java
@@ -0,0 +1,18 @@
+package com.fincheck.fincheckapijava.shared.dtos;
+
+import com.fincheck.fincheckapijava.model.Category;
+import com.fincheck.fincheckapijava.model.enums.TransactionType;
+
+import java.util.UUID;
+
+public record CategoryDto(
+        UUID id,
+        UUID userId,
+        String name,
+        String icon,
+        TransactionType type
+) {
+    public CategoryDto(Category category) {
+        this(category.getId(), category.getUserId(), category.getName(), category.getIcon(), category.getType());
+    }
+}
